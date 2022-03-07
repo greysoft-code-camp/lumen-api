2a594a06a5d553fd7ceae94eb4202d9ee2eabecf
3m1n3nc3 <mygames.ng@gmail.com>
2022-03-07 12:55:34 +0100

    Debug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -91,7 +91,6 @@ $app->routeMiddleware([
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
