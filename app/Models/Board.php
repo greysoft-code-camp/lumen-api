18bdcf16f6dc3b72ca7cb67cba4a02c724675633
3m1n3nc3 <mygames.ng@gmail.com>
2022-03-10 17:47:30 +0100

    Dummy Message

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+//Run O!
 class Board extends Model
 {
     protected $casts = [
